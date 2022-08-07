4f2f1f67b77b7bfb9173f07e285ab3168af79735
Milo Toor <milo.toor@gmail.com>
2022-08-07 15:25:53 -0700

    Polyfill String.prototype.replaceAll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+// Polyfills not provided by next.js
 import 'core-js/features/array/at';
+import 'core-js/features/string/replace-all';
+
 import 'katex/dist/katex.min.css';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
