33c503e76d07c27ac07c10844fc5c79d74c8632e
Milo Toor <milo.toor@gmail.com>
2022-08-27 21:59:51 -0700

    Add `decoration` prop to Collapse

--- components/Collapse.tsx
+++ components/Collapse.tsx
@@ -3,20 +3,25 @@ import * as React from 'react';
 
 import { ChildProp } from '../lib/types';
 
-type CollapseProps = ChildProp & { heading: string; startOpen?: boolean };
+type CollapseProps = ChildProp & {
+  decoration?: React.ReactElement;
+  heading: string;
+  startOpen?: boolean;
+};
 
-export function Collapse({ children, heading, startOpen = false }: CollapseProps) {
+export function Collapse({ children, decoration, heading, startOpen = false }: CollapseProps) {
   const [contentEl, setContentEl] = React.useState<HTMLDivElement | null>(null);
   const [open, setOpen] = React.useState(startOpen);
   const transitionClasses = 'ease-in-out duration-500';
   return (
     <div className="shadow-md shadow-slate-500 rounded-lg overflow-hidden border border-slate-400">
       <div
-        className="cursor-pointer flex items-center justify-start pl-4 py-1 hover:bg-slate-100"
+        className="cursor-pointer flex items-center justify-start pl-4 py-1 hover:bg-slate-100 w-full"
         onClick={() => setOpen((state) => !state)}
       >
         <div className={cn('text-lg transition', { 'rotate-90': open })}>›</div>
-        <div className="text-md ml-3">{heading}</div>
+        <div className="text-md ml-3 grow">{heading}</div>
+        {decoration ? <div className="justify-self-end mr-2">{decoration}</div> : null}
       </div>
       <div
         ref={(ref) => setContentEl(ref)}
