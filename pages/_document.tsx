87db381699afa9eae63acbe595e61c9192c9d36b
Milo Toor <milo.toor@gmail.com>
2022-09-04 19:18:57 -0700

    Improve iOS rendering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* These control how the app renders on the iOS home screen/in Safari mobile */}
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+        {/* Loads Roboto fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@400;700&family=Roboto+Mono:wght@300;700&display=swap"
           rel="stylesheet"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       flex: {
-        'half': '50%'
+        half: '50%',
       },
       fontFamily: {
         'fancy': ['Lobster'],
-        'roboto': ['Roboto'],
-        'roboto-mono': ['"Roboto Mono"']
+        'roboto': ['Roboto', '"Proxima Nova"'],
+        'roboto-mono': ['"Roboto Mono"', '"Courier New"', 'sans-serif'],
       },
       height: {
-        'top-bar': '3rem'
+        'top-bar': '3rem',
       },
       listStyleType: {
-        'alpha': 'lower-alpha',
+        alpha: 'lower-alpha',
       },
       maxWidth: {
-        'image': '90%'
+        image: '90%',
       },
       transitionProperty: {
-        'border': 'border-color',
-        'height': 'height'
+        border: 'border-color',
+        height: 'height',
       },
       // These values match the tailwind CSS media breakpoint values
       width: {
         large: '1024px',
-        medium: '768px'
-      }
+        medium: '768px',
+      },
     },
   },
   plugins: [],
-}
+};
