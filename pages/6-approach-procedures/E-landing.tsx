212a020dfce5697497d7c3a7d4b58837acaa45d6
Milo Toor <milo.toor@gmail.com>
2022-09-11 14:27:48 -0700

    RVR notes

--- pages/6-approach-procedures/E-landing.tsx
+++ pages/6-approach-procedures/E-landing.tsx
@@ -1,8 +1,20 @@
 import { NextPage } from 'next';
 import React from 'react';
 
-import { Bold, Danger, Italic, Link, Paragraph, TaskPage } from '../../components';
+import {
+  AIM,
+  Bold,
+  Danger,
+  Info,
+  Italic,
+  Link,
+  Paragraph,
+  Quotation,
+  TaskPage,
+  Term,
+} from '../../components';
 import { getStructure, getTaskFromSectionLetter } from '../../lib/data_loaders';
+import { uri } from '../../lib/references';
 
 export const getStaticProps = () => ({
   props: { structure: getStructure(), task: getTaskFromSectionLetter(6, 'E') },
@@ -15,6 +27,34 @@ const Landing: NextPage<TaskPage.TopLevelProps> = (props) => {
       notes={{
         // Pilot responsibilities and landing factors
         k1: [
+          <Paragraph heading="Meteorological factors" references={<AIM paragraph={[7, 1, 13]} />}>
+            A crucial consideration on every IAP so <Term>flight visibility</Term> and{' '}
+            <Term>runway visual range (RVR)</Term>.{' '}
+            <Info>
+              All approaches have a minimum visibility requirement, defined either in terms of
+              statute miles or RVR (in meters).
+            </Info>{' '}
+            <Quotation source={['FAA', references.rvr_explainer]}>
+              [RVR] is required to support precision landing and takeoff operations in the NAS. The
+              system measures visibility, background luminance, and runway light intensity{' '}
+              <Bold>
+                to determine the [horizontal] distance a pilot should be able to see down the runway
+              </Bold>
+              ...Each RVR system consists of the following functional elements: Visibility Sensor,
+              Ambient Light Sensor, Runway Light Intensity Monitor, Data Processing Unit and
+              Controller Display(s). The RVR interfaces with the Automated Surface Observing System
+              (ASOS), Enhanced Traffic Management System (ETMS), and Maintenance Processing System
+              (MPS)...
+              <Bold>
+                The lowest authorized ILS minimums...are: (a) Category I Decision Height (DH) 200
+                feet and RVR 2,400 feet (with touchdown zone and centerline lighting, RVR 1,800
+                feet).
+              </Bold>
+            </Quotation>
+            RVR values are computed by sensors mounted on poles near the runway which transmit light
+            at a known intensity and measure the extent of dissipation.
+          </Paragraph>,
+
           <Paragraph heading="Straight-In Approaches">
             A word of caution about the straight-in approach at non-towered airports:{' '}
             <Danger>be very careful to communicate clearly with VFR traffic in the pattern!</Danger>{' '}
@@ -31,6 +71,7 @@ const Landing: NextPage<TaskPage.TopLevelProps> = (props) => {
             (However, immediately following this excerpt they acknowledge that a visual approach at
             the end of an instrument approach is one such time when a straight-in may be desirable.)
           </Paragraph>,
+
           <>
             At none other than Watsonville airport (where I will meet my DPE for the instrument
             checkride) there was a{' '}
@@ -51,5 +92,6 @@ const Landing: NextPage<TaskPage.TopLevelProps> = (props) => {
 export default Landing;
 
 const references = {
+  rvr_explainer: uri.faa.nav_services('lsg/rvr'),
   wvi_accident: 'http://www.kathrynsreport.com/2022/08/cessna-340a-n740wj-fatal-accident.html',
 };
