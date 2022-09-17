68b6b91fae8edee197022d554d2918ae795b78d8
Milo Toor <milo.toor@gmail.com>
2022-09-17 15:57:16 -0700

    Overlooked Quotation changes

--- pages/6-approach-procedures/E-landing.tsx
+++ pages/6-approach-procedures/E-landing.tsx
@@ -36,24 +36,27 @@ const Landing: NextPage<TaskPage.TopLevelProps> = (props) => {
             <Info>
               All approaches have a minimum visibility requirement, defined either in terms of
               statute miles or RVR (in meters).
-            </Info>{' '}
-            <Quotation source={['FAA', references.rvr_explainer]}>
-              [RVR] is required to support precision landing and takeoff operations in the NAS. The
-              system measures visibility, background luminance, and runway light intensity{' '}
-              <Bold>
-                to determine the [horizontal] distance a pilot should be able to see down the runway
-              </Bold>
-              ...Each RVR system consists of the following functional elements: Visibility Sensor,
-              Ambient Light Sensor, Runway Light Intensity Monitor, Data Processing Unit and
-              Controller Display(s). The RVR interfaces with the Automated Surface Observing System
-              (ASOS), Enhanced Traffic Management System (ETMS), and Maintenance Processing System
-              (MPS)...
-              <Bold>
-                The lowest authorized ILS minimums...are: (a) Category I Decision Height (DH) 200
-                feet and RVR 2,400 feet (with touchdown zone and centerline lighting, RVR 1,800
-                feet).
-              </Bold>
-            </Quotation>
+            </Info>
+          </Paragraph>,
+
+          <Quotation source={['FAA', references.rvr_explainer]}>
+            [RVR] is required to support precision landing and takeoff operations in the NAS. The
+            system measures visibility, background luminance, and runway light intensity{' '}
+            <Bold>
+              to determine the [horizontal] distance a pilot should be able to see down the runway
+            </Bold>
+            ...Each RVR system consists of the following functional elements: Visibility Sensor,
+            Ambient Light Sensor, Runway Light Intensity Monitor, Data Processing Unit and
+            Controller Display(s). The RVR interfaces with the Automated Surface Observing System
+            (ASOS), Enhanced Traffic Management System (ETMS), and Maintenance Processing System
+            (MPS)...
+            <Bold>
+              The lowest authorized ILS minimums...are: (a) Category I Decision Height (DH) 200 feet
+              and RVR 2,400 feet (with touchdown zone and centerline lighting, RVR 1,800 feet).
+            </Bold>
+          </Quotation>,
+
+          <>
             RVR values are computed by sensors mounted on poles near the runway which transmit light
             at a known intensity and measure the extent of dissipation. Per{' '}
             <FAR section={[91, 175, 'h']} />{' '}
@@ -62,24 +65,28 @@ const Landing: NextPage<TaskPage.TopLevelProps> = (props) => {
               converting to ground visibility.
             </Info>{' '}
             2,400 RVR should be converted to ½SM.
-          </Paragraph>,
+          </>,
 
           <Paragraph heading="Straight-In Approaches">
             A word of caution about the straight-in approach at non-towered airports:{' '}
-            <Danger>be very careful to communicate clearly with VFR traffic in the pattern!</Danger>{' '}
-            <Quotation source={['AC 90-66B §9.5', referenceURIs['AC 90-66B']]}>
-              The FAA encourages pilots to use the standard traffic pattern when arriving or
-              departing a non-towered airport or a part-time-towered airport when the control tower
-              is not operating,{' '}
-              <Bold>
-                particularly when other traffic is observed or when operating from an unfamiliar
-                airport.
-              </Bold>
-            </Quotation>
+            <Danger>be very careful to communicate clearly with VFR traffic in the pattern!</Danger>
+          </Paragraph>,
+
+          <Quotation source={['AC 90-66B §9.5', referenceURIs['AC 90-66B']]}>
+            The FAA encourages pilots to use the standard traffic pattern when arriving or departing
+            a non-towered airport or a part-time-towered airport when the control tower is not
+            operating,{' '}
+            <Bold>
+              particularly when other traffic is observed or when operating from an unfamiliar
+              airport.
+            </Bold>
+          </Quotation>,
+
+          <>
             Immediately following this excerpt, however, it is acknowledged that a visual approach
             at the end of an instrument approach is one such time when a straight-in may be
             desirable.
-          </Paragraph>,
+          </>,
 
           <>
             At none other than Watsonville airport (where I will meet my DPE for the instrument
