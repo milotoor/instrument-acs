fbaecb162b7ee80469a11b9e63af2d88030ef3fd
Milo Toor <milo.toor@gmail.com>
2022-10-12 23:21:44 -0700

    Delete Previous/Next buttons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import React from 'react';
 
 import { Structure, Task } from '../lib/types';
@@ -6,7 +5,6 @@ import { numeralToNumber } from '../lib/util';
 import { AppContext } from './context';
 import { Link } from './Link';
 
-type MaybeTask = Structure.Task | undefined;
 type LayoutProps = {
   children: React.ReactNode;
   home?: boolean;
@@ -16,7 +14,6 @@ type LayoutProps = {
 };
 
 export function Layout({ children, home = false, section, structure, task }: LayoutProps) {
-  const tasks = React.useMemo(() => structure?.sections?.flatMap((s) => s.tasks), [structure]);
   const meta = task?.meta;
   const [sectionNumber, letter] = meta
     ? [numeralToNumber(meta.section.numeral), meta.letter]
@@ -24,42 +21,16 @@ export function Layout({ children, home = false, section, structure, task }: Lay
     ? [section.number, undefined]
     : [undefined, undefined];
 
-  const taskIndex = React.useMemo(
-    () => tasks?.findIndex((t) => t.section === sectionNumber && t.letter === letter),
-    [tasks, task]
-  );
-
-  let prevTask: MaybeTask, nextTask: MaybeTask;
-  if (typeof taskIndex === 'number') {
-    if (taskIndex !== 0) prevTask = tasks?.at(taskIndex - 1);
-    if (taskIndex !== tasks!.length - 1) nextTask = tasks?.at(taskIndex + 1);
-  }
-
-  const navLinkClasses = 'absolute h-top-bar flex flex-col justify-center font-roboto';
   return (
     <AppContext.Provider value={{ section: sectionNumber, structure, task: letter }}>
       <div className="flex h-screen flex-col items-center justify-start">
         {!home && (
           <div className="w-full h-top-bar z-10 flex-shrink-0 shadow-xl shadow-slate-800 flex flex-row justify-center items-center relative bg-gradient-to-r from-cyan-500 to-blue-500">
-            <span className={cn(navLinkClasses, 'left-5')}>
-              {prevTask && (
-                <Link bold color={null} href={prevTask.uri}>
-                  ← Previous
-                </Link>
-              )}
-            </span>
             <div className="font-fancy text-2xl hover:text-glow-gold">
               <Link color={null} href="/">
                 The Instrument ACS
               </Link>
             </div>
-            <span className={cn(navLinkClasses, 'right-5')}>
-              {nextTask && (
-                <Link bold color={null} href={nextTask.uri}>
-                  Next →
-                </Link>
-              )}
-            </span>
           </div>
         )}
         <div className="w-full pt-4 pb-2 flex-grow overflow-auto  flex flex-col items-center">
