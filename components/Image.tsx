3e76ddf31478da7e6165be9d4630a7ffa9e566df
Milo Toor <milo.toor@gmail.com>
2022-10-26 19:49:25 -0700

    Update AttributionProps so color is black by default

--- components/Image.tsx
+++ components/Image.tsx
@@ -11,9 +11,10 @@ type License = 'CC BY-SA 4.0' | 'CC BY-SA 3.0';
 type VerticalPosition = 'bottom' | 'top';
 type HorizontalPosition = 'left' | 'right' | 'center';
 type AttributionPosition = `${VerticalPosition}-${HorizontalPosition}`;
+type AttributionTextColor = 'black' | 'slate' | 'white';
 type AttributionProps = {
   author: string;
-  className?: string;
+  color?: AttributionTextColor;
   license?: License;
   link: string;
   position?: AttributionPosition;
@@ -45,7 +46,7 @@ const worksWithoutAuthors = [
 ];
 
 function Attribution(props: AttributionProps) {
-  const { author, className, license, link, position = 'bottom-left', title } = props;
+  const { author, color = 'black', license, link, position = 'bottom-left', title } = props;
 
   const authorlessWork = worksWithoutAuthors.includes(author);
   let { titleAuthorConnection } = props;
@@ -68,23 +69,25 @@ function Attribution(props: AttributionProps) {
   const [vPosition, hPosition] = position.split('-') as [VerticalPosition, HorizontalPosition];
   return (
     <div
-      className={cn(
-        className,
-        'absolute px-2 py-1 bg-black/5 backdrop-blur-sm text-xs text-white',
-        {
-          'bottom-0': vPosition === 'bottom',
-          'left-0': hPosition === 'left',
-          'top-0': vPosition === 'top',
-          'right-0': hPosition === 'right',
-          'left-1/2 translate-x-[-50%]': hPosition === 'center',
-          'rounded-tr-lg': position === 'bottom-left',
-          'rounded-t-lg': position === 'bottom-center',
-          'rounded-tl-lg': position === 'bottom-right',
-          'rounded-br-lg': position === 'top-left',
-          'rounded-b-lg': position === 'top-center',
-          'rounded-bl-lg': position === 'top-right',
-        }
-      )}
+      className={cn('absolute px-2 py-1 bg-black/5 backdrop-blur-sm text-xs', {
+        // Positioning
+        'bottom-0': vPosition === 'bottom',
+        'left-0': hPosition === 'left',
+        'top-0': vPosition === 'top',
+        'right-0': hPosition === 'right',
+        'left-1/2 translate-x-[-50%]': hPosition === 'center',
+        'rounded-tr-lg': position === 'bottom-left',
+        'rounded-t-lg': position === 'bottom-center',
+        'rounded-tl-lg': position === 'bottom-right',
+        'rounded-br-lg': position === 'top-left',
+        'rounded-b-lg': position === 'top-center',
+        'rounded-bl-lg': position === 'top-right',
+
+        // Colors
+        'text-black': color === 'black',
+        'text-slate-300': color === 'slate',
+        'text-white': color === 'white',
+      })}
     >
       {titleNode} {titleAuthorConnection} {authorNode} {licenseElement}
     </div>
@@ -153,71 +156,67 @@ const references = {
 const attributions: Record<number, Record<string, AttributionProps>> = {
   1: {
     dma_chart: {
-      className: 'text-black',
       author: 'BruceAir',
       link: references.dma_explainer,
     },
     lenticular_cloud: {
-      title: 'Mayon Volcano with cloudy hat',
       author: 'Patryk Reba',
+      color: 'white',
       license: 'CC BY-SA 4.0',
       link: 'https://commons.wikimedia.org/wiki/File:Mayon_Volcano_with_cloudy_hat.jpg',
+      title: 'Mayon Volcano with cloudy hat',
     },
   },
   2: {
     ads_b: {
       author: 'Aeronautical Information Manual',
-      className: 'text-black',
       link: uri.aim(4, 5, 7),
       title: 'Figure 4-5-7',
     },
     ads_b_display: {
       author: 'ForeFlight',
+      color: 'white',
       link: 'https://support.foreflight.com/hc/en-us/articles/4406036663063-What-traffic-features-does-ForeFlight-Mobile-offer-',
       title: 'ADS-B Traffic Targets',
     },
     efd: {
       author: 'Instrument Flying Handbook',
+      color: 'white',
       link: referenceURIs['FAA-H-8083-15'],
       title: 'Figure 6-21/6-22',
     },
     g1000_vor_id: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       position: 'bottom-center',
       title: 'Figure 4-21',
     },
     tis_block_diagram: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(4, 5, 6),
       title: 'Figure 4-5-6',
     },
     vor: {
       author: 'IFH',
-      className: 'text-slate-300',
+      color: 'slate',
       link: referenceURIs['FAA-H-8083-15'],
       position: 'bottom-right',
       title: 'Figure 9-12',
     },
     vor_principle: {
       author: 'Orion 8',
-      className: 'text-black',
       license: 'CC BY-SA 3.0',
       link: 'https://commons.wikimedia.org/wiki/File:VOR_principle.gif',
       title: 'VOR principle',
     },
     vor_service_volumes_legacy: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(1, 1, 8),
       position: 'top-left',
       title: 'Figure 1-1-1',
     },
     vor_service_volumes_new: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(1, 1, 8),
       position: 'top-left',
       title: 'Figure 1-1-4',
@@ -226,13 +225,11 @@ const attributions: Record<number, Record<string, AttributionProps>> = {
   3: {
     hold_components: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(5, 3, 8),
       title: 'Figure 5-3-3',
     },
     hold_entries: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(5, 3, 8),
       position: 'bottom-right',
       title: 'Figure 5-3-4',
@@ -241,60 +238,53 @@ const attributions: Record<number, Record<string, AttributionProps>> = {
   4: {
     pfd_scan: {
       author: 'Instrument Flying Handbook',
-      position: 'bottom-right',
+      color: 'white',
       link: referenceURIs['FAA-H-8083-15'],
+      position: 'bottom-right',
       title: 'Figure 6-33',
     },
   },
   6: {
     cdi_diagram: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       title: 'Figure 2-19',
     },
     cdi_enunciations: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       position: 'bottom-right',
       title: 'Table 2-1',
     },
     gps_cdi_scaling_phases: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       position: 'bottom-right',
       title: 'Figure 2-23 Automatic CDI Scaling',
     },
     ils_diagram: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(1, 1, 9),
       title: 'FAA Instrument Landing Systems',
     },
     lnav_cdi_scaling: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       title: 'Figure 2-24',
     },
     localizer_limitations: {
       author: 'AIM',
-      className: 'text-black',
       link: uri.aim(1, 1, 9),
       title: 'Limits of Localizer Coverage',
     },
     lpv_cdi_scaling: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       position: 'bottom-right',
       title: 'Figure 2-25',
     },
     raim_prediction: {
       author: "G1000 Pilot's Guide",
-      className: 'text-black',
       link: referenceURIs.g1000,
       position: 'top-right',
       title: 'Figure 5-107 RAIM Prediction',
