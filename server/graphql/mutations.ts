7b9c287214c1fed2e87c5ff5a21d55679c04c272
Milo Toor <milo.toor@gmail.com>
2022-12-01 15:25:59 -0800

    Remove unused GraphQL mutations

--- server/graphql/mutations.ts
+++ server/graphql/mutations.ts
@@ -17,33 +17,3 @@ export const createContactMessage = /* GraphQL */ `
     }
   }
 `;
-export const updateContactMessage = /* GraphQL */ `
-  mutation UpdateContactMessage(
-    $input: UpdateContactMessageInput!
-    $condition: ModelContactMessageConditionInput
-  ) {
-    updateContactMessage(input: $input, condition: $condition) {
-      id
-      email
-      subject
-      message
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const deleteContactMessage = /* GraphQL */ `
-  mutation DeleteContactMessage(
-    $input: DeleteContactMessageInput!
-    $condition: ModelContactMessageConditionInput
-  ) {
-    deleteContactMessage(input: $input, condition: $condition) {
-      id
-      email
-      subject
-      message
-      createdAt
-      updatedAt
-    }
-  }
-`;
