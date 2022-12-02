3e3953fa422f9cd02b520a2028a7613ee9e42980
Milo Toor <milo.toor@gmail.com>
2022-12-01 21:46:52 -0800

    Test new SES email source

--- amplify/backend/function/instrumentacsd0795340/src/index.js
+++ amplify/backend/function/instrumentacsd0795340/src/index.js
@@ -1,12 +1,6 @@
 const aws = require('aws-sdk');
 const ses = new aws.SES();
 
-// I tried very, very hard to keep this as an environment variable, but Amplify just doesn't seem to
-// support that when using git-based CI/CD. It could work with manual deployments instead, except
-// that it doesn't support manual deployments for apps requiring SSR! Amplify really doesn't seem to
-// be production ready...
-const contactEmail = 'milo.toor@gmail.com';
-
 /**
  * Whenever a new ContactMessage object is added to the database, we will send an email (using SES)
  * to the above email address
@@ -17,8 +11,8 @@ exports.handler = async (event) => {
       const { email, message, subject } = record.dynamodb.NewImage;
       await ses
         .sendEmail({
-          Destination: { ToAddresses: [contactEmail] },
-          Source: contactEmail,
+          Destination: { ToAddresses: ['milo.toor@gmail.com'] },
+          Source: 'contact@milo.aero',
           ReplyToAddresses: [email.S],
           Message: {
             Subject: { Data: `Instrument ACS contact: ${subject.S}` },
