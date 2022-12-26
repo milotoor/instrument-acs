76401ae261b8dcbe478b88d0e7fa42cb0b465e9e
Milo Toor <milo.toor@gmail.com>
2022-12-26 15:05:01 -0800

    Dramatically simplify JSON structure, construction

--- scripts/build_far_json.py
+++ scripts/build_far_json.py
@@ -3,16 +3,46 @@ from pathlib import Path
 import requests
 
 base_url = "https://www.ecfr.gov/api/versioner/v1/"
-child_key = "children"
-description_key = "label_description"
-id_key = "identifier"
-range_separator = " – "
+cited_sections = {
+    43: [9],
+    61: [3, 23, 51, 57, 113],
+    68: [3, 7],
+    91: [
+        3,
+        21,
+        109,
+        121,
+        126,
+        144,
+        155,
+        159,
+        167,
+        169,
+        171,
+        173,
+        175,
+        177,
+        185,
+        205,
+        207,
+        211,
+        213,
+        215,
+        225,
+        227,
+        403,
+        409,
+        411,
+        413,
+    ],
+    95: [15],
+}
 
 
 class FARJSONBuilder:
     """
-    Builds the JSON for each FAR part of interest. In order to link to the sections cited within our application we need
-    to know some extra information about where to find its text within the CFR as a whole. For example, FAR 91.3
+    Builds the JSON for each section of 14 CFR cited in our application. In order to link to the sections we need to
+    know some extra information about where to find its text within the CFR as a whole. For example, FAR 91.3
     (Responsibility and authority of the pilot in command) is found at the following URL:
 
         https://www.ecfr.gov/current/title-14/chapter-I/subchapter-F/part-91/subpart-A/section-91.3
@@ -23,141 +53,100 @@ class FARJSONBuilder:
     a JSON structure ordered like so:
 
       {
-        "43": {
-          "description": "Maintenance, Preventive Maintenance, Rebuilding, and Alteration",
-          "chapter": "I",
-          "subchapter": "C",
-          "sections": {
-            "1": "Applicability.",
-            ...
-          }
-        },
-        "61": {
-          ...
-          "subparts": {
-            "A": {
-              ...
-              "section_range": [1, 60],
-              "sections": {
-                "1": "Applicability and definitions.",
-                ...
-              }
-            },
-            ...
-          }
-        },
+        "43.1": [
+          { "id": "C", "description": "Aircraft", "type": "subchapter" },
+          {
+            "id": "43",
+            "description": "Maintenance, Preventive Maintenance, Rebuilding, and Alteration",
+            "type": "part",
+          },
+          { "id": "1", "description": "Applicability.", "type": "section" }
+        ],
+
         ...
+
+        "61.3": [
+          { "id": "D", "description": "Airmen", "type": "subchapter" },
+          {
+            "id": "61",
+            "description": "Certification: Pilots, Flight Instructors, and Ground Instructors",
+            "type": "part",
+          },
+          { "id": "A", "description": "General", "type": "subpart" },
+          {
+            "id": "61.3", "description": "Requirement for certificates, ratings, and authorizations.", "type": "section"
+          }
+        ]
+
+        ...
+
+        "91.101": [
+          { "id": "F", "description": "Air Traffic and General Operating Rules", "type": "subchapter" },
+          { "id": "91", "description": "General Operating and Flight Rules", "type": "part" },
+          { "id": "B", "description": "Flight Rules", "type": "subpart" },
+          { "id": "ECFRe4c59b5f5506932", "description": "General", "type": "subject_group" },
+          { "id": "91.101", "description": "Applicability.", "type": "section" }
+        ]
       }
 
-    The top level contains the parts of interest. Nested under that is some metadata as well as subparts and sections.
-    If a part has no subparts (e.g. part 43) it contains the sections directly; if it does (e.g. part 61), it lists its
-    subparts and each subpart lists its contained sections.
+    The top level contains the sections of interest. Nested under each is an array of identifiers, one identifier per
+    parent in the CFR ontology leading to that section. Each identifier contains three keys: "type" (indicating what
+    sort of parent is identified, i.e. "subchapter", "part", "subpart", etc.), "description" (the name of the identified
+    parent) and "id" (the parent's ID). Together these describe precisely how to access the cited FAR section within the
+    eCFR website. (Note that the CFR title (14) and chapter ("I") are omitted because they are the same across all
+    citations)
 
-    To generate this structure we make two requests. The first is to `/api/versioner/v1/titles.json` to determine the
-    most recent version of title 14. With that information we then query for the entire structure of title 14 and winnow
-    it down to only the parts of interest.
+    To generate this structure we make one request per cited section, using the eCFR API endpoint
+    `/api/versioner/v1/ancestry/{date}/title-{title}.json`. For instance, to look up FAR 91.175 we would use the URL:
+
+        https://www.ecfr.gov/api/versioner/v1/ancestry/2022-12-22/title-14.json?part=91&section=91.175
+
+    Importantly, the `date` parameter must first be looked up independently using the `/api/versioner/v1/titles.json`
+    endpoint. This will yield the publication date of the most recent version of title 14. After that it's just a matter
+    of querying for each cited section and trimming the results.
     """
 
-    def __init__(self, parts: list[int]):
-        self.parts = parts
+    def __init__(self):
+        self.issue_date = requests.get(base_url + "titles.json").json()["meta"]["date"]
+        self.json = {"subchapters": {}, "parts": {}, "subparts": {}, "subject_groups": {}, "sections": {}}
 
-        issue_date = requests.get(base_url + "titles.json").json()["meta"]["date"]
-        title_14 = requests.get(base_url + f"structure/{issue_date}/title-14.json").json()
+    def build_section(self, part: int, section: int):
+        uri = base_url + f"ancestry/{self.issue_date}/title-14.json?part={part}&section={part}.{section}"
+        section_ancestry = requests.get(uri).json()["ancestors"]
 
-        # All parts of interest are contained in chapter 1 of title 14
-        self.json = title_14[child_key][0]
+        section_identifiers = []
+        for i, ancestor in enumerate(section_ancestry):
+            anc_type = ancestor["type"]
+            anc_id = ancestor["identifier"]
+            anc_desc = ancestor["label_description"]
 
-    @property
-    def subchapters(self):
-        return self.json[child_key]
+            # The title is always 14 and chapter always I. The part is included in the citation and the subchapter is
+            # identifiable from the part. The section is handled differently and includes its description, not its ID.
+            if anc_type in ["subpart", "subject_group"]:
+                section_identifiers.append(anc_id)
 
-    @staticmethod
-    def get_children(json_obj, child_type: str):
-        return [child for child in json_obj[child_key] if child["type"] == child_type]
-
-    @staticmethod
-    def get_range(json_obj):
-        descendant_range = json_obj["descendant_range"]
-
-        def drop_prefix(range_str):
-            return int(range_str.split(".")[-1] if "." in range_str else range_str)
-
-        if range_separator in descendant_range:
-            start, end = json_obj["descendant_range"].split(range_separator)
-            start = drop_prefix(start)
-            end = drop_prefix(end)
-        else:
-            descendant_range = drop_prefix(descendant_range)
-            start = end = descendant_range
-        return start, end
-
-    @classmethod
-    def get_sections(cls, children: list) -> dict:
-        sections = {}
-        subject_groups = {}
-
-        for child in children:
-            child_type = child["type"]
-            if child_type == "section" and not child["reserved"]:
-                section_num = child[id_key].split(".")[-1]
-                sections[section_num] = child[description_key]
-            # Some subparts are further divided into "subject groups"--see part 91, section B
-            elif child_type == "subject_group":
-                subject_groups[child[id_key]] = cls.get_range(child)
-                sections.update(cls.get_sections(child[child_key])["sections"])
-
-        section_dict = {"sections": sections}
-        if subject_groups:
-            section_dict["subject_groups"] = subject_groups
-        return section_dict
-
-    def build_part(self, part_num: int):
-        part, subchapter = self.find_part(part_num)
-        part_dict = {"chapter": "I", "description": part[description_key], "subchapter": subchapter[id_key]}
-
-        subparts = self.get_children(part, "subpart")
-        if subparts:
-            part_dict["subparts"] = {
-                subpart[id_key]: {
-                    "description": subpart[description_key],
-                    "section_range": self.get_range(subpart),
-                    **self.get_sections(subpart[child_key]),
-                }
-                for subpart in subparts
-            }
-        else:
-            # If there are no subparts there should be "sections" instead
-            part_dict.update(self.get_sections(part[child_key]))
-
-        return part_dict
-
-    def find_part(self, part_num: int):
-        # First find the subchapter containing it
-        subchapter_with_part = None
-        for subchapter in self.subchapters:
-            part_range = self.get_range(subchapter)
-            if part_range[0] <= part_num <= part_range[1]:
-                subchapter_with_part = subchapter
-                break
-
-        err = f"Unable to find part {part_num}"
-        if not subchapter_with_part:
-            raise ValueError(err)
-
-        subchapter_parts = subchapter_with_part[child_key]
-        for part in subchapter_parts:
-            if int(part[id_key]) == part_num:
-                return part, subchapter_with_part
-        raise ValueError(err)
+            if anc_type in ["subchapter", "subject_group"]:
+                self.json[anc_type + "s"][anc_id] = anc_desc
+            # The part includes which subchapter it's in. The subchapter is always one above the part
+            elif anc_type == "part":
+                self.json["parts"][anc_id] = [section_ancestry[i - 1]["identifier"], anc_desc]
+            elif anc_type == "subpart":
+                self.json["subparts"][f"{part}.{anc_id}"] = anc_desc
+            # The section specifies its subpart and subject group (if applicable) and appends its description
+            elif anc_type == "section":
+                self.json["sections"][anc_id] = section_identifiers + [anc_desc]
 
     def generate(self):
         """Save the results to the data directory"""
-        far_data = {part: self.build_part(part) for part in self.parts}
+        for part, sections in cited_sections.items():
+            for section in sections:
+                self.build_section(part, section)
+
         data_dir = Path(__file__).parent.parent / "data"
         with open(data_dir / "far.json", "w") as f:
-            json.dump(far_data, f, indent=2)
+            json.dump(self.json, f, indent=2)
 
 
 if __name__ == "__main__":
-    builder = FARJSONBuilder(parts=[43, 61, 68, 91, 95])
+    builder = FARJSONBuilder()
     builder.generate()
