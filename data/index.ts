0c138faa50efd7f32bfa981ec02ce15503a16b54
Milo Toor <milo.toor@gmail.com>
2022-12-26 20:23:33 -0800

    Fix TypeScript's bad inference

--- data/index.ts
+++ data/index.ts
@@ -3,4 +3,14 @@ import aimJson from './aim.json';
 import farJson from './far.json';
 
 export const aim: Data.AIM = aimJson;
-export const far = farJson as Data.FAR;
+
+// TypeScript is performing an unhelpful type inference on the farJson object, leading it to
+// complain that:
+//
+//   The types of 'parts[43]' are incompatible between these types
+//     Type 'string[]' is not comparable to type 'Part.Content'
+//       Target requires 2 element(s) but source may have fewer
+//
+// In other words it is inferring the type of parts[43] to be string[] and then complains when I
+// attempt to declare its type as  [string, string]. Thus the cast to unknown
+export const far = farJson as unknown as Data.FAR;
